1d66f0415558cbf736970c17e6a67797a2d8ab74
Joshua Berkowitz <berkowitz.jt@gmail.com>
2021-11-29 16:04:23 -0500

    login form checkpoint
    Beginning to add login functionality to project.
    
    Added:
    - login.js
    - login.html
    - login.py
    
    The plan is to lump together login and signup features into one python file since they serve similar purposes. Using mongodb for the backend. Eventually may need a way to mask the statements writing to the database.
    
    Modified:
    - layout.html

--- flaskr/routes/home.py
+++ flaskr/routes/home.py
@@ -9,3 +9,13 @@ from flaskr import app
 @app.route('/home')
 def home():
     return(render_template('home.html'))
+
+
+# prevent cached responses
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
